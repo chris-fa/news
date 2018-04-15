85ee3832cc14a0b38d48f485fa2a5ac2ce948586
Christopher Francisco <christopher.f.almanza@gmail.com>
2018-04-15 11:25:00 -0500

    Create server skeleton and Dockerfile

---
+++ server/src/routes/about.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.json({
+    version: process.env.npm_package_version,
+  });
+});
+
+module.exports = router;
